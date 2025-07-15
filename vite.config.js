d5fe4f10ec0073b5f585e4b8894f2a890d75640f
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-07-15 12:39:27 +0300

    hv

--- vite.config.js
+++ vite.config.js
@@ -4,46 +4,18 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    base: '/goit-js-hw-10/',
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('*.html', { cwd: 'src', absolute: true }),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        timer: resolve(__dirname, 'src/1-timer.html'),
+        snackbar: resolve(__dirname, 'src/2-snackbar.html'),
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+  },
+  base: '/goit-js-hw-10/',
+  ...
 });
